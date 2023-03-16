f53cc975d339bf26a94ab0793d5ccebfde2678a5
Rex <proandceo@gmail.com>
2023-03-16 15:08:23 +1000

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -30,7 +30,7 @@ class BudgetList:
         try:
             return self.iter_e.__next__()
         except StopIteration as stop:
-            except return self.iter_o.__next__()
+            return self.iter_o.__next__()
 
 def main():
     myBudgetList = BudgetList(1200)
